0348e1f833101c86c22574386f296ca7de2e87dc
Kuznetsov-I-N-TSA-142 <160126755+Kuznetsov-I-N-TSA-142@users.noreply.github.com>
2024-06-20 09:42:16 +0300

    Update task4.cpp

--- task4.cpp
+++ task4.cpp
@@ -3,55 +3,44 @@
 #include <iomanip>
 using namespace std;
 
-/*
-@brief Проверяет положительные ли значения
+/**
+ * @brief Проверяет положительные ли значения
  * @param n-число
- * @return- значение n
+ * @return значение n
 */
 void checkN(const double n);
-
-/*
-@brief ввод значений с проверкой
- * @return- числа
+/**
+ * @brief ввод значений с проверкой
+ * @return числа
 */
 double getNumber();
-
-/*
-@brief табулирует заданную в таблице функцию
+/**
+ * @brief табулирует заданную в таблице функцию
  * @param minValue-минимальное значение функции
  * @param maxValue-максимальное значение функции
  * @param step-шаг
- * @return- выводит таблицу
+ * @return выводит таблицу
 */
 void printTabFunction(const double minValue, const double maxValue, const double step);
-
-/*
+/**
  * @brief проверяет что минимум меньше максимума
  * @param min-минимальное значение функции
  * @param max-максимальное значение функции
  * @return- значения
 */
 void checkRange(const double min, const double max);
-
 /**
- * @brief Проверяет, является ли переданное целое число большим или равным нулю.
- * @param i Целое число, которое требуется проверить.
- * @return true, если переданное число больше или равно нулю, в противном случае возвращает false.
- */
-bool isGreaterThanOrEqualToNegativeZero(int i);
-
-/**
-* @breef - главная функция программы
+* @brief - главная функция программы
 * @return - возвращает 0, если функция выполнена верно, иначе 1.
 */
 int main()
 {
-    cout << "Введите минимальный предел: ";
+    cout << "Введите минимальный предел->";
     double minValue = getNumber();
-    cout << "Введите максимальный предел: ";
+    cout << "Введите максимальный предел->";
     double maxValue = getNumber();
     checkRange(minValue, maxValue);
-    cout << "Введите шаг: ";
+    cout << "Введите шаг->";
     double step = getNumber();
     checkN(step);
     printTabFunction(minValue, maxValue, step);
@@ -61,21 +50,15 @@ int main()
 
 void printTabFunction(const double minValue, const double maxValue, const double step)
 {
-    if (minValue + step > maxValue) {
-        cout << "Шаг выходит за пределы максимального значения." << endl;
-        return;
-    }
-
     cout << setw(10) << "x" << setw(15) << "y" << endl;
     for (double i = minValue; i <= maxValue; i += step)
     {
-        if (i > maxValue)
-        {
-            cout << "Значение x выходит за пределы максимального значения." << endl;
-            break;
+        if (i < 0) {
+            cout << setw(10) << i << " Функция неопределена, под корнем отрицательное значение или ноль!" << endl;
+        } else {
+            double a = 3 * sin(sqrt(i)) + 0.39 * i - 3.8;
+            cout << setw(10) << i << setw(15) << a << endl;
         }
-        double a = 3 * sin(sqrt(i)) + 0.39 * i - 3.8;
-        cout << setw(10) << i << setw(15) << a << endl;
     }
 }
 
@@ -86,7 +69,7 @@ double getNumber()
     if (cin.fail())
     {
         cout << "Incorrect input";
-        exit(1);
+        abort();
     }
     return a;
 }
@@ -96,7 +79,7 @@ void checkN(const double n)
     if (n <= 0)
     {
         cout << "Wrong value" << endl;
-        exit(1);
+        abort();
     }
 }
 
@@ -105,11 +88,6 @@ void checkRange(const double min, const double max)
     if (min > max)
     {
         cout << "Wrong range entered" << endl;
-        exit(1);
+        abort();
     }
-}
-
-bool isGreaterThanOrEqualToNegativeZero(int i)
-{
-    return i >= 0;
 }
