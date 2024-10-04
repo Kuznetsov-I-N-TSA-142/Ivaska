cd9a2fa58e6970ec02d174a3b3e8c248f2bad55e
Kuznetsov-I-N-TSA-142 <160126755+Kuznetsov-I-N-TSA-142@users.noreply.github.com>
2024-10-04 13:19:40 +0300

    Update task5.cpp

--- task5.cpp
+++ task5.cpp
@@ -4,52 +4,20 @@
 
 using namespace std;
 
-/**
-*@brief Считывает значение с клавиатуры с проверкой ввода
-* @return Введённое значение
-*/
 double getValue();
-
-/**
-*@brief Проверяет число на то, чтобы оно не было равно нулю или меньше нуля
-* @param value - Введённое значение
-*/
-void checkValue(double const value);
-
-/**
-*@brief Производит расчёт суммы ряда функции от 1 до n
-* @param n - Число ряда, до которого будет посчитана сумма ряда
-* @return Сумма ряда функций
-*/
+void checkValue(double const value, bool strict);
 double getSum(const int n);
-
-/**
-*@brief Подсчёт рекуррентной функции
-* @param current - прошлое значение функции
-* @param i - порядковый номер ряда
-* @return Полученное значение функции
-*/
 double getNext(const double current, const int i);
-
-/**
-*@brief Производит расчёт суммы ряда функции от 1 до n
-* @param e - Число, до которого будет подсчитываться сумма ряда
-* @return Сумма ряда функций до определённого числа
-*/
 double getSumE(double const e);
 
-/**
-*@brief - главная функция программы
-* @return - возвращает 0, если функция выполнена верно, иначе 1.
-*/
 int main()
 {
     cout << "Enter the number of elements = ";
     int a = static_cast<int>(getValue());
-    checkValue(a);
+    checkValue(a, true); // строгое сравнение
     cout << "Enter the element from which the sequence will begin except 0 = ";
     double b = getValue();
-    checkValue(b);
+    checkValue(b, false); // нестрогое сравнение
     cout << "The sum of the first elements = " << getSum(a) << endl;
     cout << "Sum of elements after tracked number = " << getSumE(b) << endl;
     return 0;
@@ -66,12 +34,23 @@ double getValue()
     return n;
 }
 
-void checkValue(double const value)
+void checkValue(double const value, bool strict)
 {
-    if (value <= 0) // Проверка на <= 0 вместо < 0
+    if (strict)
     {
-        cout << "error" << endl;
-        abort();
+        if (value <= 0) // Проверка на <= 0
+        {
+            cout << "error" << endl;
+            abort();
+        }
+    }
+    else
+    {
+        if (value < 0) // Проверка на < 0
+        {
+            cout << "error" << endl;
+            abort();
+        }
     }
 }
 
